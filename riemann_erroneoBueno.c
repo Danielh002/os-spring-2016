f32a08b4062d323f980d036f88ffd7d393c38f39
Danielh002 <danielhernandez002@hotmail.com>
2016-04-26 19:49:07 -0500

    Segunda carga

--- riemann_erroneoBueno.c
+++ riemann_erroneoBueno.c
@@ -17,49 +17,47 @@
 #define MAXTHREAD 10
 #define BASE 0.0
 #define LIMIT 1.0
-//Tipo de dato sem_t y gestions de hilos
-#include <semaphore.h> 
 
-sem_t sem;
 double length = LIMIT - BASE;
 double numRectxThread = (double)(MAXRECT/MAXTHREAD);
 double base_length = 0;
-double sumTotal = 0;
+
+double sumTotalVector[MAXTHREAD];
 
 double function(double x) {
 	return x * x; 
 }
 void* calcular(void *arg) {
+	double sumTotal = 0;
 	long id = (long) arg;
 	double lowlimit = id*base_length*numRectxThread;
-	double localSum=0;
+
 
 	for (int i = 0; i < numRectxThread; i++) {
-		
-		localSum += function(lowlimit + i*base_length) * base_length;
-		
+		sumTotal += function(lowlimit + i*base_length) * base_length;
 	}
-	sem_wait(&sem);
-	sumTotal+=localSum;
-	sem_post(&sem);
+	sumTotalVector[id]=sumTotal;
 	return 0;
 }
 
 int main(int argc, char** argv) {
 	pthread_t threads[MAXTHREAD];
+	double sumTotal =0;
 	long taskids[MAXTHREAD];
 
 	base_length = length/MAXRECT;
-	sem_init(&sem,0,1);
 	printf("base length: %lf numRectxThread: %lf\n",base_length, numRectxThread);
 	for (int i = 0; i < MAXTHREAD; i++) {
 		taskids[i] = i;
 		pthread_create(&threads[i], NULL, calcular, (void*)taskids[i]);
 	}
-	for (int i = 0; i < MAXTHREAD; i++) {
-		pthread_join(threads[i],NULL);
+	for( int i=0; i< MAXTHREAD ; i++){
+		pthread_join( threads[i], NULL );
 	}
-	sem_destroy(&sem);
+	for( int i=0; i < MAXTHREAD ; i++){
+		sumTotal+=sumTotalVector[i];
+	}
 	printf("Suma total %lf\n",sumTotal);
 	pthread_exit(NULL);
+
 }
